1649254665c951833b616c15e78c650980f69411
zqi16341-spec <277940847+zqi16341-spec@users.noreply.github.com>
2026-05-14 22:43:44 +0800

    更新Java学习记录

---
+++ JavaBasic/src/main/java/com/github/qichangfeng/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.github.qichangfeng;
+//输出一段文本：helloWorld
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
--- JavaBasic/src/main/java/practice/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package practice;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
